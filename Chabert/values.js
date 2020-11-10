e51dcc925bb1118f51593a2a247cb1ef9f03b790
EmperorMollusk <misterscratch@gmail.com>
2020-11-09 20:42:20 -0500

    Updating values to be a name and description array

--- Chabert/values.js
+++ Chabert/values.js
@@ -1,21 +1,21 @@
-var bingoValues = [
-"Lacey Chabert"
-, "Winnie Cooper"
-,"Red-Haired Chick"
-, "Single Parent"
-, "Conveniently Dead Spouse"
-, "Exile to Small Town America"
-, "Name Brand Actor"
-, 'You\'ve been "It\'s a Wonderful Life"d'
-, "Heroine Owns the Business"
-, "Job in Danger"
-, "Save the Farm!"
-, "Sassy BFF"
-, "DJ, from the Full House"
-, "...Then, Santa Shows Up"
-, "Childhood Dreams Come True"
-, "Falling for the Enemy"
-, "Groundhog Day"
+let bingoValues = [
+{n: "Lacey Chabert", d: "An appearance of the Christmas Queen"}
+, {n:"Winnie Cooper", d: "Remember \"The Wonder Years\"? Well so does she."}
+, {n:"Red-Haired Chick", d: "Our star is that Red-Haired Chick, not that one the other one."}
+, {n:"Single Parent", d: "All the cute of a kid with none of the romantic entaglement"}
+, {n:"Conveniently Dead Spouse", d: "Family tragedy paving the way to Yultide love"}
+, {n:"Exile to Small Town America", d:"I'm a big city girl how could I ever learn to love your small-town ways?"}
+, {n:"Name Brand Actor", d:"And then, right in the middle of this mess, we see Ed Asner."}
+, {n:"You\'ve been \"It\'s a Wonderful Life\"d\'", d: "You learn a lesson about that path not taken and Clarence gets his wings."}
+, {n:"Heroine Owns the Business", d:"She's just trying to keep that bookstore open!"}
+, {n:"Job in Danger", d:"I want pictures of Mommy kissing Santa Claus on my desk by five or you're fired, Parker!"}
+, {n:"Save the Farm!", d:"The bank'll foreclose if we don't grow the best gingerbread crop the valley has ever seen!"}
+, {n:"Sassy BFF", d:"We all know what you need, girrrrrrrrl!"}
+, {n:"DJ, from the Full House", d:"What would Uncle Jesse think?"}
+, {n:"...Then, Santa Shows Up", d:"I wonder if that old man with the beard and the love of cookies has any insight."}
+, {n:"Childhood Dreams Come True", d:"Ever since I was a little girl I always to run with the bulls!"}
+, {n:"Falling for the Enemy", d: "Oh you jerk! You handsome, hunky, slab of jerk!"}
+, {n:"Groundhog Day", d: "This all seems familiar. Maybe... Too familiar."}
 , "Fancy Woman Packs Wrong"
 , "Childhood Sweethearts Reconnect"
 , "Christmas is Cancelled... But Wait!"
@@ -40,4 +40,6 @@ var bingoValues = [
 , "Spent All the Money on The Actors"
 , "Sleigh Ride with Animal Cruelty"
 , "Town Square Make Out"
-, "Downton Abbey Staff Christmas"]
+, "Downton Abbey Staff Christmas"
+, "The Store Brand"
+]
